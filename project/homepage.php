60c39e927380ad504e25bc035c0b260d003d9ac3
Jake Laurence De Guzman Paz <91404725+Jakelaurencepaz@users.noreply.github.com>
2024-06-07 06:40:29 +0400

    Add files via upload

---
+++ project/homepage.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Arise</title>
+    <link rel="stylesheet" href="homepage.css">
+</head>
+<body>
+    <section class="header">
+        <nav>
+            <a href="homepage.php"><img src="Images/newweblogo.png"></a>
+            <div class="nav-links">
+                <ul>
+                    <li><a href="homepage.html">HOME</a></li>
+                    <li><a href="aboutus.html">ABOUT US</a></li>
+                    <li><a href="projects.html">PROJECTS</a></li>
+                    <li><a href="contact.html">CONTACT</a></li>
+                    <li><a href="login.php">LOGOUT</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="text-box">
+            <h1><strong>THE FUTURE IS IN OUR HANDS</strong></h1>
+            <P>Welcome to ARISE. Where we focus on encouraging joy, creativity, and collaboration.
+                We blend the boundaries <br>between indoor and outdoor, it a method to improve freedom and spontaneity of space,
+                bringing our people with <br>enjoyment. We find possibilities for fresh configurations and interiors for shifting future dynamics.
+            </P>
+            <a href="aboutus.html" class="hmp-btn">Learn More About Us</a>
+        </div>
+    </section>
+
+    <!---PROJ HOMEPAGE--->
+
+    <section class="projects">
+        <h2><strong>Our Projects</strong></h2>
+        <p>This is where you belong</p>
+
+        <div class="row">
+            <div class="projects-col">
+                <img src="Images/proj1.jpg" width="100%;">
+            </div>
+            <div class="projects-col">
+                <img src="Images/proj2.jpg" width="99.7%;">
+            </div>
+            <div class="projects-col">
+                <img src="Images/proj3.jpg" width="100%;">
+            </div>
+        </div>
+
+    </section>
+
+    <!--MOREINFO HOMEPAGE-->
+
+    <section class="info">
+        <h3><strong>Know More About Us</strong></h3>
+        <p>We welcome you to our humble home</p>
+
+        <div class="row">
+            <div class="info-col">
+                <img src="Images/info1.jpg" width="100%;">
+                <h4>ARISE headqaurters</h4>
+                <p>Welcome to the headqaurters of ARISE. Which is located in United Kingdom, Surrey.
+                Here in ARISE headqaurters we have over 700+ employees
+                who are filled with creativity</p>
+            </div>
+            <div class="info-col">
+                <img src="Images/info2.jpg" width="99.9%;">
+                <h4>Break Room</h4>
+                <p>Here in ARISE, with over 700 employees we focus on our employees physical 
+                and mental health. Here in ARISE headqaurters we have a dedicated room where 
+                employees can unwind.</p>
+            </div>
+            <div class="info-col">
+                <img src="Images/info3.jpg" width="99.9%;">
+                <h4>Conference Room</h4>
+                <p>Our conference is dedicated to meetings, workshops, etc. 
+                in this room we express our creativity and widen our proficiency.</p>
+            </div>
+        </div>
+
+    </section>
+
+    <!--CONTACTUS HOMEPAGE-->
+
+    <section class="contact">
+        <h5>Let us redefine the way you live</h5>
+        <a href="contact.html" class="hmp-btn">Contact Us</a>
+    </section>
+
+    <!--SOCIALMEDIA HOMEPAGE-->
+
+    <footer>
+        <h6><strong>Thank you for visiting ARISE</strong></h6>
+        <p>Come join us! As team ARISE is constantly making lives better <br /> and redefing the households of one another.</p>
+        <p>
+            <a class="social-icon" href="https://www.linkedin.com/in/jake-laurence-paz-873317286/">
+                <ion-icon name="logo-linkedin"></ion-icon>
+            </a>
+
+            <a class="social-icon" href="https://www.instagram.com/jakelaurencepaz/?hl=en">
+                <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+
+            <a class="social-icon" href="https://github.com/Jakelaurencepaz">
+                <ion-icon name="logo-github"></ion-icon>
+            </a>
+
+            <a class="social-icon" href="https://pin.it/2J9364P">
+                <ion-icon name="logo-pinterest"></ion-icon>
+            </a>
+        </p>
+    </footer>
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</body>
+</html>
